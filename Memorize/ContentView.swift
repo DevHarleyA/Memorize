7585851547f79fa4744763985d621a069938c10c
DevHarleyA <aharleydev@gmail.com>
2023-02-19 23:04:39 -0500

    complete additional themes, need todo hw xc

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,16 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    var emojis = ["✈️", "🚗", "🚂", "🚀", "🚌", "🚙", "🚕", "🏎", "🚜", "🚐", "🛻", "🚒", "🚚", "🚛", "🏍", "🛸", "🚁", "🚆", "🚎", "🚓", "🛵", "🛳", "🚘", "🚖 "]
-    @State var emojiCount = 20
+    @State var emojis = ["✈️", "🚗", "🚂", "🚀", "🚌", "🚙", "🚕", "🏎", "🚜", "🚐", "🛻", "🚒", "🚚", "🚛", "🏍", "🛸", "🚁", "🚆", "🚎", "🚓", "🛵", "🛳", "🚘", "🚖 "]
+    @State var emojiCount = Int.random(in: 8...20)
 
     var body: some View {
         VStack {
             ScrollView {
+                Text("Memorize")
+                    .font(.largeTitle)
                 HStack {
                     // LazyVGrid uses all width horizontally, vertically it makes the cards as small as possible so it can make as many as possible
                     // GridItems allows you to set adaptive so it looks good in portrait and landscape mode
-                    LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    LazyVGrid(columns:
+                            // TODO: Try to come up with some sort of equation that relates the number of cards in the game to the width you pass when you create your LazyVGrid’s GridItem(.adaptive(minimum:maximum:)) such that each time a theme button is chosen, the LazyVGrid makes the cards as big as possible without having to scroll.
+                            [GridItem(.adaptive(minimum: 65))]) {
                         // id helps uniquely identify each view the foreach makes
                         ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
                             CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
@@ -27,10 +31,13 @@ struct ContentView: View {
             }
             .foregroundColor(.red)
             Spacer()
-            HStack {
-                remove
-                Spacer()
-                add
+            HStack(spacing: 20) {
+                animalTheme
+                natureTheme
+                vehicleTheme
+//                remove
+//                Spacer()
+//                add
             }
             .font(.largeTitle)
             .padding(.horizontal)
@@ -56,6 +63,48 @@ struct ContentView: View {
         } label: {
             // use images from SF Symbols
             Image(systemName: "plus.circle")
+        }
+    }
+    
+    var animalTheme: some View {
+        Button {
+            emojis = ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼", "🐻‍❄️", "🐨", "🐯", "🦁", "🐮", "🐷", "🐸", "🦄", "🐥", "🐙", "🐢", "🦀"]
+            emojis.shuffled()
+            emojiCount = Int.random(in: 8...20)
+            
+        } label: {
+            VStack {
+                Text("Animal\n Theme")
+                    .font(.subheadline)
+                Image(systemName: "pawprint.circle")
+            }
+        }
+    }
+    
+    var natureTheme: some View {
+        Button {
+            emojis = ["🌵", "🌲", "🌳", "🌴", "🪵", "🌱", "☘️", "🍀", "🍁", "🌷", "🌺", "🌸", "🌼", "🌙", "❄️", "☀️", "🌿", "🪴", "🍄", "🌹"]
+            emojis.shuffled()
+            emojiCount = Int.random(in: 8...20)
+        } label: {
+            VStack {
+                Text("Nature\n Theme")
+                    .font(.subheadline)
+                Image(systemName: "sun.and.horizon.circle")        }
+        }
+    }
+    
+    var vehicleTheme: some View {
+        Button {
+            emojis = ["✈️", "🚗", "🚂", "🚀", "🚌", "🚙", "🚕", "🏎", "🚜", "🚐", "🛻", "🚒", "🚚", "🚛", "🏍", "🛸", "🚁", "🚆", "🚎", "🚓", "🛵", "🛳", "🚘", "🚖 "]
+            emojis.shuffled()
+            emojiCount = Int.random(in: 8...20)
+        } label: {
+            VStack {
+                Text("Vehicle\n Theme")
+                    .font(.subheadline)
+                Image(systemName: "car.circle")
+            }
         }
     }
 }
