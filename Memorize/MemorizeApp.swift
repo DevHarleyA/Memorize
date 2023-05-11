e38151a3a05804f761bbdecdb1d71e42e70c6c2d
DevHarleyA <aharleydev@gmail.com>
2023-05-10 22:52:20 -0400

    not much code, learn about xcode

--- Memorize/MemorizeApp.swift
+++ Memorize/MemorizeApp.swift
@@ -7,11 +7,11 @@
 
 import SwiftUI
 
-@main
+@main // main program
 struct MemorizeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView() // This is the thing that describes what our app looks like
         }
     }
 }
