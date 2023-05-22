b7e28430c1ecdd9356968c854a175a732f30e395
DevHarleyA <aharleydev@gmail.com>
2023-05-22 07:46:00 -0400

    finish class three

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -1,8 +1,25 @@
 //
 //  EmojiMemoryGame.swift
-//  Memorize
+//  Memorize View Model
 //
 //  Created by Alexandra Harley on 5/21/23.
 //
 
 import SwiftUI
+
+class EmojiMemoryGame {
+    static let emojis = ["🇯🇲", "🇫🇷", "🇺🇸", "🇰🇷", "🇬🇧", "🏳️‍🌈", "🇧🇸", "🇨🇦", "🇧🇷", "🇧🇪", "🇨🇮", "🇪🇨", "🇨🇺", "🇮🇹", "🇮🇷", "🇯🇵", "🇰🇪", "🇳🇴", "🇿🇦", "🇸🇪", "🇹🇹", "🇺🇦", "🇰🇳", "🇪🇸"] // static makes it global, but it's nested. Allows it to be initialized
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame(numberOfPairsOfCards: 4) { pairIndex in
+                emojis[pairIndex]
+        }
+    }
+    
+    private var model: MemoryGame<String> = createMemoryGame()
+     // "private" protects the model from view
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+}
